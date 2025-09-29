bc8a4a928fde992a5dc63449289e3d29c85dc077
daravathsreeja <sreekanthdaravath@gmail.com>
2025-09-29 21:10:35 +0530

    Update auth.py

--- Backend/app/routers/auth.py
+++ Backend/app/routers/auth.py
@@ -1,1 +1,46 @@
 
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from app import schemas, database_models, utils
+from app.database import session
+
+router = APIRouter()
+
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/register", response_model=schemas.UserOut)
+def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(database_models.User).filter(database_models.User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="This email is already registered.")
+
+    hashed_pw = utils.hash_password(user.password)
+    db_user = database_models.User(
+        username=user.username, 
+        email=user.email,
+        hashed_password=hashed_pw
+    )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+@router.post("/login", response_model=schemas.Token)
+def login_user(user: schemas.UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(database_models.User).filter(database_models.User.email == user.email).first()
+
+    if not db_user or not utils.verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token = utils.create_access_token(data={"sub": db_user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
