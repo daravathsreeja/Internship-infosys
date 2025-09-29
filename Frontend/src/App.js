0efc882cb36392c32b5932b35693cdb966c50d06
daravathsreeja <sreekanthdaravath@gmail.com>
2025-09-29 21:15:12 +0530

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Routes , Route, Navigate } from "react-router-dom";
+import Home from "./Components/home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
